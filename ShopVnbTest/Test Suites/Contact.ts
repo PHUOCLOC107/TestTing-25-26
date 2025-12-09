302e152c982fedc1427f7ff0f2e3709ca64a696c
Lê Văn Luận <22130156@st.hcmuaf.edu.vn>
2025-12-09 22:03:29 +0700

    up

--- ShopVnbTest/Test Suites/Contact.ts
+++ ShopVnbTest/Test Suites/Contact.ts
@@ -15,21 +15,21 @@
       <guid>fe9dd11e-6a20-421e-a5da-5a0982fa9013</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Contact/TC02_FullInfo_SendSuccess</testCaseId>
+      <testCaseId>Test Cases/TC_Contact/TC02_FullInfo_SendSuccess</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>97b173de-795f-4234-b5a0-ee9928492b26</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Contact/TC03_MissingField</testCaseId>
+      <testCaseId>Test Cases/TC_Contact/TC03_MissingField</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>5721fe71-62d9-457a-895c-b64e34db16c5</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Contact/TC04_InvalidFormat</testCaseId>
+      <testCaseId>Test Cases/TC_Contact/TC04_InvalidFormat</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
--- ShopVnbTest/Test Suites/StoreSystem.ts
+++ ShopVnbTest/Test Suites/StoreSystem.ts
@@ -15,28 +15,28 @@
       <guid>7e47813c-0b4b-4c0c-8862-9e427e15d8f5</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/StoreSystem/TC02_Filter_Full</testCaseId>
+      <testCaseId>Test Cases/TC_StoreSystem/TC02_Filter_Full</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>516f62ac-eb53-423b-9d90-7061f17e5097</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/StoreSystem/TC03_Filter_MissingDistrict</testCaseId>
+      <testCaseId>Test Cases/TC_StoreSystem/TC03_Filter_MissingDistrict</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>33442ac7-a577-40ae-a6f9-497091274b71</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/StoreSystem/TC04_Filter_OnlyDistrict</testCaseId>
+      <testCaseId>Test Cases/TC_StoreSystem/TC04_Filter_OnlyDistrict</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>06447bc5-8cab-4582-ab5e-3ad51456f2b7</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/StoreSystem/TC05_View_StoreDetail</testCaseId>
+      <testCaseId>Test Cases/TC_StoreSystem/TC05_View_StoreDetail</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
