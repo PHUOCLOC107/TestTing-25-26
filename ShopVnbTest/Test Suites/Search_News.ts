950e45ae9afdc0e6bbdaf574ab87605dc35aa081
nguyenvubao1601 <22130024@st.hcmuaf.edu.vn>
2025-12-10 21:38:48 +0700

    Update Testcase News Script

---
+++ ShopVnbTest/Test Suites/Search_News.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>search_news</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>955e5a5a-5ede-48da-b393-4859aede0109</testSuiteGuid>
+   <testCaseLink>
+      <guid>d031ef2a-2412-4a59-8526-1ebab29e43dd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/search_news/test1</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>41333adb-6d70-4151-a9f4-33551243b50f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/search_news/test2</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>67c75436-e355-4b3e-bdc3-3c2f93faf014</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/search_news/test3</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>89af730b-82db-44e0-a8ae-c356fc7171b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/search_news/test4</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>c429f6a4-c2c5-4d7e-99ba-8e74af87f7bf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/search_news/test5</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
---
+++ ShopVnbTest/Test Suites/View_News.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>view_news</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d7f0255b-f6e6-40de-960b-2c8373185eb4</testSuiteGuid>
+   <testCaseLink>
+      <guid>505bac71-1637-49dc-8654-fd33f9e63e7f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/view_news/test1</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>bfcfef3e-c94d-4d75-b248-3c6492eccfcf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/view_news/test2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
